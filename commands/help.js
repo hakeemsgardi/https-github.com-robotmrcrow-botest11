b4e9618df167155a443b3f1a126c16e13b88358c
robotmrcrow <73116672+robotmrcrow@users.noreply.github.com>
2020-11-14 20:51:01 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,35 +1,27 @@
 const { MessageEmbed } = require("discord.js");
 
-client.on('message', message => {
-if (message.content.startsWith(prefix + "help")) 
- 
-    
-{
-  var SUPPORT = "https://discord.gg/PN6VQUc";
-  var Link = "https://discord.com/api/oauth2/authorize?client_id=761634046840275005&permissions=8&scope=bot";
-  var Web = "https://discord.gg/Ra8dJ4t";
-let embed = new Discord.RichEmbed()
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  description: "Display all commands and descriptions",
+  execute(message) {
+    let commands = message.client.commands.array();
 
+    let helpEmbed = new MessageEmbed()
+      .setTitle(`${message.client.user.username} Help`)
+      .setDescription("List of all commands")
+      .setColor("#F8AA2A");
 
-.setThumbnail(message.author.avatarURL)
-.setTitle('Command Help Menu' )
-.setDescription(`** **[ Support](${SUPPORT})** - **[ Invite](${Link})**
- 
-welcome and left
-\`craete channel: welcome\`
-\`craete channel: left\`
-Generall
-\`g/server\`- \`g/infobot\`-\`g/bots\`-\`g/myrole\`- 
-\`g/invite\` - \`g/vote\` - \`g/support\` - \`g/member\` - \`g/roles\` - \`g/emoji\` - \`g/user\`
-Moderation
-\`g/lock\` - \`g/unlock\` -\`g/setnick\` - 
-\`g/clear\`
-**`)
- 
- 
-.setColor('#3BA9FF')
-      .setTimestamp()
-  .setFooter(message.author.username, message.author.avatarURL)
- .setAuthor(`${message.author.username}`, `${message.author.avatarURL}`);
-message.channel.sendEmbed(embed);
-    }
+    commands.forEach((cmd) => {
+      helpEmbed.addField(
+        `**${message.client.prefix}${cmd.name} ${cmd.aliases ? `(${cmd.aliases})` : ""}**`,
+        `${cmd.description}`,
+        true
+      );
+    });
+
+    helpEmbed.setTimestamp();
+
+    return message.channel.send(helpEmbed).catch(console.error);
+  }
+};
