87a5b9978e940e3692a2181f817e54b6d3de787b
robotmrcrow <73116672+robotmrcrow@users.noreply.github.com>
2020-11-14 19:58:33 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,6 +8,16 @@ module.exports = {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
+    {
+  var SUPPORT = "https://discord.gg/PN6VQUc";
+  var Link = "https://discord.com/api/oauth2/authorize?client_id=761634046840275005&permissions=8&scope=bot";
+  var Web = "https://discord.gg/Ra8dJ4t";
+let embed = new Discord.RichEmbed()
+
+
+.setThumbnail(message.author.avatarURL)
+.setDescription(`** **[ Support](${SUPPORT})** - **[ Invite](${Link})**
+ 
       .setTitle(`${message.client.user.username} Help`)
       .setDescription("List of all commands")
       .setColor("#F8AA2A");
