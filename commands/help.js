63e635a3d58ca7f0f69b01af480dc88b55c82131
robotmrcrow <73116672+robotmrcrow@users.noreply.github.com>
2020-11-14 21:21:38 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -16,7 +16,8 @@ module.exports = {
         "Link invite Bot ::blue_heart:"
       )
       .setURL(
-        "https://discord.com/api/oauth2/authorize?client_id=755768592104030208&permissions=8&scope=bot"
+       "https://discord.com/api/oauth2/authorize?client_id=755768592104030208&permissions=8&scope=bot"
+        );
       .setThumbnail(message.author.avatarURL)
       .setColor("#F8AA2A");
       
