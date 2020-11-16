2344809a1cc5c00a8b04f7a1f8a61e27e8d9b6db
robotmrcrow <73116672+robotmrcrow@users.noreply.github.com>
2020-11-17 01:33:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -35,6 +35,43 @@ client.user.setActivity(`${PREFIX}help ,Users ${client.guilds.cache.reduce((a, g
 }, 15000);
 
 });
+
+client.on('message', message => {
+if (message.content.startsWith(prefix + "help1")) 
+ 
+    
+{
+  var SUPPORT = "https://discord.gg/PN6VQUc";
+  var Link = "https://discord.com/api/oauth2/authorize?client_id=761634046840275005&permissions=8&scope=bot";
+  var Web = "https://discord.gg/Ra8dJ4t";
+let embed = new Discord.RichEmbed()
+
+
+.setThumbnail(message.author.avatarURL)
+.setTitle('Command Help Menu' )
+.setDescription(`** **[ Support](${SUPPORT})** - **[ Invite](${Link})**
+ 
+welcome and left
+\`craete channel: welcome\`
+\`craete channel: left\`
+Generall
+\`g/server\`- \`g/infobot\`-\`g/bots\`-\`g/myrole\`- 
+\`g/invite\` - \`g/vote\` - \`g/support\` - \`g/member\` - \`g/roles\` - \`g/emoji\` - \`g/user\`
+Moderation
+\`g/lock\` - \`g/unlock\` -\`g/setnick\` - 
+\`g/clear\`
+**`)
+ 
+ 
+.setColor('#3BA9FF')
+      .setTimestamp()
+  .setFooter(message.author.username, message.author.avatarURL)
+ .setAuthor(`${message.author.username}`, `${message.author.avatarURL}`);
+message.channel.sendEmbed(embed);
+    }
+});
+
+
 client.on("warn", (info) => console.log(info));
 client.on("error", console.error);
 
