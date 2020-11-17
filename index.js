abe72349e7605603a51259465fa9746a96d1ea75
robotmrcrow <73116672+robotmrcrow@users.noreply.github.com>
2020-11-17 22:26:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -83,23 +83,7 @@ client.on("message", async (message) => {
       );
     }
   }
-  const now = Date.now();
-  const timestamps = cooldowns.get(command.name);
-  const cooldownAmount = (command.cooldown || 3) * 1000;
-
-  if (timestamps.has(message.author.id)) {
-    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-    if (now < expirationTime) {
-      const timeLeft = (expirationTime - now) / 1000;
-      return message.reply(
-        `please wait ${timeLeft.toFixed(
-          1
-        )} more second(s) before reusing the \`${command.name}\` command.`
-      );
-    }
-  }
-
+  
   timestamps.set(message.author.id, now);
   setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
